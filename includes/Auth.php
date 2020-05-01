b30486c3b7d409357231885ea60c28bf0f4d92f3
Alexander <a.khodyrev@intensa.ru>
2020-05-01 12:00:04 +0300

    отключил авторизацию на уровне php

--- includes/Auth.php
+++ includes/Auth.php
@@ -6,24 +6,23 @@ class Auth
 {
     public function needAuth()
     {
-        $settings = \Includes\Settings::getInstance();
-        $users = $settings->users();
-        $needAuth = true;
-
-        header('Cache-Control: no-cache, must-revalidate, max-age=0');
-        $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
-
-        if ($has_supplied_credentials) {
-            foreach ($users as $name => $user) {
-                if ($name == $_SERVER['PHP_AUTH_USER'] && $user['password'] == $_SERVER['PHP_AUTH_PW']) {
-                    exit;
-                }
-            }
-        } else {
-            header('HTTP/1.1 401 Authorization Required');
-            header('WWW-Authenticate: Basic realm="Access denied"');
-            exit;
-        }
+//        $settings = \Includes\Settings::getInstance();
+//        $users = $settings->users();
+//
+//        header('Cache-Control: no-cache, must-revalidate, max-age=0');
+//        $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
+//
+//        if ($has_supplied_credentials) {
+//            foreach ($users as $name => $user) {
+//                if ($name == $_SERVER['PHP_AUTH_USER'] && $user['password'] == $_SERVER['PHP_AUTH_PW']) {
+//                    exit;
+//                }
+//            }
+//        } else {
+//            header('HTTP/1.1 401 Authorization Required');
+//            header('WWW-Authenticate: Basic realm="Access denied"');
+//            exit;
+//        }
 //        $_COOKIE
     }
 
